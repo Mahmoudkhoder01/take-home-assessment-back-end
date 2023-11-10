51a49a79f780d3ac6422f0f98d6cdd4fe8b404a3
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-11-10 13:38:13 +0200

    fix the problem of the token by changing the findUnique to findFirst in jwt.strategy

--- src/authentication/jwt.strategy.ts
+++ src/authentication/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: { email: string }) {
-    const users = await this.prismaService.user.findUnique({
+    const users = await this.prismaService.user.findFirst({
       where: {
         email: payload.email,
       },

001f5e7945518957e6c7d04ac63140d1d6589ac7
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-11-12 21:51:54 +0200

    add the auth UseGuardes to the todo contollers

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -8,10 +8,12 @@ import {
   Put,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { TodoService } from './todo.service';
 import { Todo } from '@prisma/client';
+import { JwtAuthGuard } from 'src/authentication/auth.guard';
 
 @Controller('todo')
 export class TodoController {
@@ -32,6 +34,7 @@ export class TodoController {
   }
 
   @Get('remaining/:userId')
+  @UseGuards(JwtAuthGuard)
   async getRemainingTodosForUser(
     @Param('userId') userId: number,
   ): Promise<Todo[]> {
@@ -39,6 +42,7 @@ export class TodoController {
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   async createTodo(
     @Body() postData: Todo,
     @Req() req: Request,
@@ -55,6 +59,7 @@ export class TodoController {
   }
 
   @Put()
+  @UseGuards(JwtAuthGuard)
   async updateTodo(
     @Body() data: { id: number; todo: Todo },
     @Req() req: Request,
@@ -75,6 +80,7 @@ export class TodoController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   async deleteTodo(
     @Param('id') id: number,
     @Req() req: Request,
